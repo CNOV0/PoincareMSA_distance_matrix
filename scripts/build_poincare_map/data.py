692f367bbc6f25a8289a94fb9faf075d609db0c3
CNOV0 <mayazygadlo@DESKTOP-TARTU7S.localdomain>
2022-11-22 23:43:22 +0100

    Adding true globin_colors_new.csv and script to print it.

--- scripts/build_poincare_map/data.py
+++ scripts/build_poincare_map/data.py
@@ -160,11 +160,11 @@ def compute_rfa(features, distfile, mode='features', k_neighbours=15, distfn='sy
     # precomputed matrix
     else:
         # distance_matrix = pairwise.cosine_distances(features, Y=None)
-        distance_matrix = pairwise.euclidean_distances(features, Y=None)
+        # distance_matrix = pairwise.euclidean_distances(features, Y=None)
         # read distance matrix
-        #distance_matrix = pd.read_csv(distfile)
-        #distance_matrix = distance_matrix.drop(['0', 'Unnamed: 0'], axis=1)
-        #distance_matrix = distance_matrix.drop(distance_matrix.index[231], axis=0)
+        distance_matrix = pd.read_csv(distfile)
+        distance_matrix = distance_matrix.drop(['0', 'Unnamed: 0'], axis=1)
+        distance_matrix = distance_matrix.drop(distance_matrix.index[231], axis=0)
         # construct graph
         knn_distance_based = NearestNeighbors(n_neighbors=k_neighbours,
                                 metric="precomputed").fit(distance_matrix)
